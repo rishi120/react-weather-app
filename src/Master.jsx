61432b62491d848bce81f6baa572e58944d35537
Chandan <chandan.b@pacewisdom>
2021-06-19 21:45:39 +0530

    added fonts and scss setup

---
+++ src/Master.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+import gsap, { Power2 } from "gsap";
+import Axios from "axios";
+import { baseUrl } from "./Components/Landing/Utils/Baseurl";
+import Renderindex from "./Components/Landing/Index";
+
+const Rendermastercomponent = () => {
+  return <Renderindex />;
+};
+
+export default Rendermastercomponent;
